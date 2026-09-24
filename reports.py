f549fe917a28ce375414cd56a53c1ef8435c0e4f
arfatkhatik <arfatkhatik5@gmail.com>
2026-09-24 16:19:49 +0530

    implement profit report

--- reports.py
+++ reports.py
@@ -22,6 +22,8 @@ def report():
         inventory_report()
     elif user_option in ("3", "customer report"):
         customer_report()
+    elif user_option in ("4", "profit report"):
+        profit_report()
 
 
 def sales_report():
@@ -69,6 +71,7 @@ def inventory_report():
     low_stock_units = 0
     out_of_stock = 0
     total_inventory_value = 0
+
     for product in products:
         total = product["stock quantity"]
         total_stock_units += total
@@ -131,3 +134,54 @@ def customer_report():
         ========================================
 
         """)
+
+
+def profit_report():
+    products = storage.load_products()
+    orders = storage.load_orders()
+
+    if not orders:
+        print("No sales found.")
+        return
+
+    total_orders = len(orders)
+
+    total_units = 0
+    total_revenue = 0
+    total_cost = 0
+
+    for order in orders:
+        for item in order["items"]:
+            revenue = item["price"] * item["quantity"]
+            total_revenue += revenue
+            total_units += item["quantity"]
+            for product in products:
+                if product["product id"] == item["product_id"]:
+                    total_cost += product["purchase price"] * item["quantity"]
+
+    total_profit = total_revenue - total_cost
+    if total_revenue > 0:
+        profit_margin = (total_profit / total_revenue) * 100
+    else:
+        profit_margin = 0
+
+
+
+
+
+
+
+    print(f"""
+    ========================================
+             PROFIT REPORT
+    ========================================
+
+    Total Orders        : {total_orders}
+    Total Units Sold    : {total_units}
+    Total Revenue       : ₹{total_revenue}
+    Total Cost          : ₹{total_cost}
+    Total Profit        : ₹{total_profit}
+    Profit Margin       : {profit_margin}%
+
+    ========================================
+    """)
